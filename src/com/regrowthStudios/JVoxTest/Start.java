a083226ce01058275ab66b8e3deda3dd9648bacb
Dylan S <adtdeveloping@gmail.com>
2015-08-24 18:33:29 +0200

    Added more tests

--- src/com/regrowthStudios/JVoxTest/Start.java
+++ src/com/regrowthStudios/JVoxTest/Start.java
@@ -10,21 +10,25 @@ import org.newdawn.slick.opengl.Texture;
 
 import com.regrowthStudios.JVox.audio.SoundManager;
 import com.regrowthStudios.JVox.entity.Entity;
-import com.regrowthStudios.JVox.graphics.Camera;
+import com.regrowthStudios.JVox.graphics.Camera2D;
 import com.regrowthStudios.JVox.graphics.SpriteBatch;
 import com.regrowthStudios.JVox.graphics.Window;
 import com.regrowthStudios.JVox.math.Vector;
 import com.regrowthStudios.JVox.math.Vector4;
+import com.regrowthStudios.JVox.physics.CollisionManager;
 import com.regrowthStudios.JVox.systems.content.ResourceSystem;
 import com.regrowthStudios.JVox.systems.particle.ParticleEmitter;
 import com.regrowthStudios.JVox.ui.CheckBox;
+import com.regrowthStudios.JVox.utils.EventUtils.KeyboardEvents;
 import com.regrowthStudios.JVox.utils.EventUtils.MouseEvents;
+import com.regrowthStudios.JVox.world.ChunkManager2D;
 
 public class Start {
     public static SoundManager soundManager = null;
-    private static Camera camera = new Camera();
+    private static Camera2D camera = new Camera2D();
     private static ArrayList<ParticleEmitter> particleEmitters = new ArrayList<ParticleEmitter>();
-
+    private static CollisionManager cManager = new CollisionManager();
+    
     public static void main(String[] args) {
         Window window = new Window();
         window.init("Test", new Vector(800, 600));
@@ -43,8 +47,10 @@ public class Start {
             ResourceSystem.loadResource("textures.button.up", "data/textures/up.png", 0);
             ResourceSystem.loadResource("textures.button.down", "data/textures/down.png", 0);
             ResourceSystem.loadResource("textures.particle.spark", "data/textures/spark.png", 0);
-            
-            ResourceSystem.loadResource("shaders.basic.texture", "./data/shaders/BasicTexture.vert, ./data/shaders/BasicTexture.frag", 1);
+            ResourceSystem.loadResource("textures.misc.empty", "data/textures/empty.png", 0);
+
+            ResourceSystem.loadResource("shaders.basic.texture",
+                    "./data/shaders/BasicTexture.vert, ./data/shaders/BasicTexture.frag", 1);
         }
 
         SpriteBatch batch = new SpriteBatch();
@@ -53,18 +59,23 @@ public class Start {
         CheckBox testContainer = new CheckBox();
         testContainer.init(new Vector4(0, 0, 40, 40));
 
-        Entity testEntity = new Entity(0, new Vector4(100, 100, 50, 50));
-
         camera.init((int) window.getDimensions().x, (int) window.getDimensions().y);
 
         byte color[] = { (byte) 255, (byte) 255, (byte) 255, (byte) 255 };
         Texture tex = (Texture) ResourceSystem.getResource("textures.background.space");
 
+        ChunkManager2D testManager = new ChunkManager2D(camera);
+        testManager.init();
+
+        float speed = 5.0F;
+
+        Entity e1 = new Entity(0, new Vector4(250, -250, 50, 50));
+        Entity player = new Entity(1, new Vector4(0, 0, 50, 50));
+        
         while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
             GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
             camera.update();
-
             Vector mp = MouseEvents.getPosition();
             mp = camera.convertScreenToWorld((float) mp.x, (float) mp.y);
 
@@ -72,14 +83,32 @@ public class Start {
             batch.draw(tex.getTextureID(), 0.0f, 0.0f, (float) window.getDimensions().x, (float) window.getDimensions().y,
                     0.0f, 0.0f, tex.getWidth(), tex.getHeight(), 0.0f, color);
 
+            testManager.update();
+            testManager.render(batch);
+
+            {
+                if (KeyboardEvents.keyDown(Keyboard.KEY_A)) {
+                    camera.setPosition((float) camera.getPosition().x - speed, (float) camera.getPosition().y);
+                } else if (KeyboardEvents.keyDown(Keyboard.KEY_D)) {
+                    camera.setPosition((float) camera.getPosition().x + speed, (float) camera.getPosition().y);
+                }
+
+                if (KeyboardEvents.keyDown(Keyboard.KEY_S)) {
+                    camera.setPosition((float) camera.getPosition().x, (float) camera.getPosition().y - speed);
+                } else if (KeyboardEvents.keyDown(Keyboard.KEY_W)) {
+                    camera.setPosition((float) camera.getPosition().x, (float) camera.getPosition().y + speed);
+                }
+            }
+
             if (MouseEvents.buttonDown(0)) {
                 ParticleEmitter emitter = new ParticleEmitter();
                 emitter.init((Texture) ResourceSystem.getResource("textures.particle.spark"), (float) mp.x, (float) mp.y, 0.0f,
                         1.0f);
-                emitter.spawnAngle = (float) Math.PI / 32.0f;
+                emitter.spawnAngle = (float) Math.PI / 128.0f;
                 emitter.particlesPerSpawn = 1;
                 emitter.framesUntilSpawn = 1;
-                emitter.setColor((byte) new Random().nextInt(255), (byte) new Random().nextInt(255), (byte) new Random().nextInt(255), (byte) 255);
+                emitter.setColor((byte) new Random().nextInt(255), (byte) new Random().nextInt(255),
+                        (byte) new Random().nextInt(255), (byte) 255);
                 particleEmitters.add(emitter);
             }
 
@@ -90,9 +119,22 @@ public class Start {
 
             testContainer.draw(batch);
             testContainer.update(MouseEvents.getMove(), mp);
+            
+            {
+                player.aabb.x = camera.getPosition().x;
+                player.aabb.y = camera.getPosition().y;
+                
+                e1.render(batch);
+                e1.update();
 
-            testEntity.render(batch);
-            testEntity.update();
+                player.render(batch);
+                player.update();
+                
+                ArrayList<Entity> e = new ArrayList<Entity>();
+                e.add(player);
+                e.add(e1);
+                cManager.collide(e);
+            }
 
             batch.end();
             batch.render(camera);
